c9ff414476e47220980d3823cee6b274151a771a
Kristopher Windsor <kristopherwindsor@gmail.com>
2018-11-25 15:13:04 -0800

    📷("🌈");

--- src/index.php
+++ src/index.php
@@ -6,7 +6,7 @@ include('inc/functions.php');
 pic("home/wff");
 pic("home/flag_mural");
 pic("home/kid_w_chick");
-pic("home/barn-rainbow");
+📷("🌈");
 pic("home/suess");
 pic("home/goodbye-cow");
 pic("home/roosters");
