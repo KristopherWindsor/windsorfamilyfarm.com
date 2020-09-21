dc32344830d3e24f90c152dcee75e456c54b898d
Kristopher Windsor <kristopherwindsor@gmail.com>
2020-09-20 22:13:23 -0700

    Update parties.php
    Remove some old pics

--- src/parties.php
+++ src/parties.php
@@ -3,14 +3,11 @@
 include('summer/summer.php');
 include('inc/functions.php');
 
-pic("tours/feed-the-ponies");
 📷("alpaca/alpaca-card");
 pic("tours/kids-on-tour");
 pic("tours/kid-with-a-bunny");
 pic("tours/farm-camp");
 pic("tours/farm-camp-craft");
-pic("tours/4h-planting");
-pic("tours/tractor");
 📷("🎪");
 📷("playground");
 
