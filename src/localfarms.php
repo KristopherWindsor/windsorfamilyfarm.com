76ce7d8e8527b00194ab160c5629ae5f9788b9d0
Kristopher Windsor <kristopherwindsor@gmail.com>
2021-03-08 23:38:17 -0800

    Create localfarms.php

---
+++ src/localfarms.php
@@ -0,0 +1,15 @@
+<?php
+
+include('summer/summer.php');
+include('inc/functions.php');
+
+pic("tours/4h-planting");
+pic("tours/tractor");
+
+start('localfarms');
+
+show_content('main', 'localfarms-main');
+
+finish();
+
+?>
