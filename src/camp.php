afd74de530b3af2662741f37b4e6bdfe71c81e54
Kristopher Windsor <kristopherwindsor@gmail.com>
2022-05-01 16:40:09 -0700

    Create camp.php

---
+++ src/camp.php
@@ -0,0 +1,27 @@
+<?php
+
+include('summer/summer.php');
+include('inc/functions.php');
+
+pic("tours/kid_feeds_cow");
+pic("tours/kids_feed_cow");
+pic("tours/camp-100");
+pic("tours/camp-101");
+pic("tours/camp-102");
+pic("tours/hold-a-bunny");
+pic("tours/touch-a-cow");
+pic("tours/feed-a-sheep");
+pic("tours/playhouse");
+pic("tours/feed-the-ponies");
+pic("tours/kids-on-tour");
+pic("tours/kid-with-a-bunny");
+pic("tours/farm-camp");
+pic("tours/farm-camp-craft");
+pic("tours/4h-planting");
+pic("tours/tractor");
+
+start('camp');
+
+show_content('main', 'camp-main');
+
+finish();
